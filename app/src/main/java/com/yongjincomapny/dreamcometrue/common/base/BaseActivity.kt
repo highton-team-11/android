fd6a0d385a96b84d1d06fa7420f7d0de19bb1ee2
lee <dragon02229@naver.com>
2024-02-17 16:55:30 +0900

    databinding 초기 설정

---
+++ app/src/main/java/com/yongjincomapny/dreamcometrue/common/base/BaseActivity.kt
@@ -0,0 +1,27 @@
+package com.yongjincomapny.dreamcometrue.common.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        init()
+        observe()
+    }
+
+    open fun init() {}
+    open fun observe() {}
+}
