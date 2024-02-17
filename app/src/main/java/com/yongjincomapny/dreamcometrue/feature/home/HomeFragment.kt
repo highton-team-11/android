853221af95a7fbad9f5857a76d794badd606b1ec
wsi1212 <wsi1212@dgsw.hs.kr>
2024-02-18 01:19:46 +0900

    Add: Home banner

--- app/src/main/java/com/yongjincomapny/dreamcometrue/feature/home/HomeFragment.kt
+++ app/src/main/java/com/yongjincomapny/dreamcometrue/feature/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.yongjincomapny.dreamcometrue.feature.home
 
-import android.view.View
 import com.yongjincomapny.dreamcometrue.R
 import com.yongjincomapny.dreamcometrue.common.base.BaseFragment
 import com.yongjincomapny.dreamcometrue.databinding.FragmentHomeBinding
@@ -8,7 +7,7 @@ import com.yongjincomapny.dreamcometrue.feature.home.adapter.DetailImageAdapter
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun init() {
-        val imageAdapter = DetailImageAdapter(listOf(R.drawable.black_50_bg, R.drawable.ic_launcher_background)) {}
+        val imageAdapter = DetailImageAdapter(listOf(R.drawable.banner1, R.drawable.banner2, R.drawable.banner3)) {}
         binding.viewpager.adapter = imageAdapter
         binding.wormDotsIndicator.attachTo(binding.viewpager)
         super.init()
